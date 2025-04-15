fd00acb5ef722b5710f16fbea36a0a3d308308be
dev0062 <zaindevalldino@gmail.com>
2025-04-15 10:59:04 +0700

    mendeklarasikan aray dengan ukuran dan diberi nilai

--- Array.cpp
+++ Array.cpp
@@ -4,3 +4,7 @@ using namespace std;
 //deklarasi array tanpa ukuran, wajib langsung diisi data
 int nilai[]={1,2,3};
 
+//deklarasi array dengan ukuran, dan diberi nilai awal
+int skor[4]={9,13,20,11};
+
+
