948455b3b3de7b1d59d88d1d2c5b8327b08a4342
dev0062 <zaindevalldino@gmail.com>
2025-04-15 11:34:35 +0700

    menampilkan data struct

--- StructGetLine.cpp
+++ StructGetLine.cpp
@@ -28,6 +28,15 @@ int main(){
     cout << "Masukkan Alamat Kota = ";
     cin >> mhs.alamat.kota;
 
+   
     
+        //menampilkan data struct
+        cout << endl;
+        cout << "NIM = " << mhs.nim << endl;
+        cout << "Nama = " << mhs.nama << endl;
+        cout << "Desa = " << mhs.alamat.desa << endl;
+        cout << "Kota = " << mhs.alamat.kota << endl;
+    
+    }
 
     
